10659ccfd15fb0ebecff6ded8b14b7c270655e2e
Chitiz Gupta <chitiz86@gmail.com>
2017-04-11 18:13:20 +0530

    Db Migration Changes

--- db/migrate/20170411040109_create_articles.rb
+++ db/migrate/20170411040109_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
-    	
+    	t.string :title
+    	t.text :body
     	t.timestamps
+    	# t.timestamps adds the two columns to the db: created_at, updated_at
     end
   end
 end
